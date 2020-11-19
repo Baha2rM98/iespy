d2b8152e6e8f1e88c551bb79e83c3b3ad73cad6f
Baha2r <baha2r.mirzazadeh98@gmail.com>
2020-11-20 02:32:58 +0330

    Getting ready for pip

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sapy-Baha2rM98",
+    version="1.0.0",
+    author="Bahador Mirzazadeh",
+    author_email="bahador.mirzazadeh30@gmail.com",
+    description="A python package for solving 'Interval Estimation' problems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Baha2rM98/sapy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
